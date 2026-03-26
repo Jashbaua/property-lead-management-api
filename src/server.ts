f1e588d08e482196a916809da0374e27c9cde011
Jashbaua <128972302+Jashbaua@users.noreply.github.com>
2026-03-26 22:28:38 +0530

    feat: add base middleware and global error handling

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
+import 'dotenv/config'; 
 import express, { Request, Response } from 'express';
+import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
 });
+
+app.get('/error', (req: Request, res: Response) => {
+  throw new Error('test');
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
