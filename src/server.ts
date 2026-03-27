fd35b267137033be497610a1758a0b7c21a5402c
Jashbaua <128972302+Jashbaua@users.noreply.github.com>
2026-03-27 09:52:13 +0530

    feat: add JWT authentication middleware

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import { errorHandler } from './middleware/errorHandler';
 import authRoutes from './routes/auth.routes';
+import { authenticate } from './middleware/auth.middleware';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,15 @@ app.use('/auth', authRoutes);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
 });
+
+app.get('/test-auth', authenticate, (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: 'Protected route',
+    user: req.user 
+  });
+});
+
 
 app.use(errorHandler);
 
